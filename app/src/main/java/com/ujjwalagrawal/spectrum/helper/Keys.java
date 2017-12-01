d93c8f5ce7c151d29ffe3b22e3110affeea6e9a4
Shubham Raj <shubhamismrocker@gmail.com>
2017-12-01 19:45:17 +0530

    trying to add button in toolbar

--- app/src/main/java/com/ujjwalagrawal/spectrum/helper/Keys.java
+++ app/src/main/java/com/ujjwalagrawal/spectrum/helper/Keys.java
@@ -9,5 +9,6 @@ public class Keys {
     public static final String KEY_MOBILE ="mobile" ;
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
+    public static final String Refer_Code="code";
 
 }
