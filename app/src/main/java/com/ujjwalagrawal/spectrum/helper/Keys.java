bf0f025f05e59c26100d78a3690f637aeb37fbca
Shubham Raj <shubhamismrocker@gmail.com>
2017-12-07 00:32:23 +0530

    ng tttttt "trying to add button in toolbar"
    This reverts commit d93c8f5ce7c151d29ffe3b22e3110affeea6e9a4.

--- app/src/main/java/com/ujjwalagrawal/spectrum/helper/Keys.java
+++ app/src/main/java/com/ujjwalagrawal/spectrum/helper/Keys.java
@@ -9,6 +9,5 @@ public class Keys {
     public static final String KEY_MOBILE ="mobile" ;
     public static final String KEY_NAME = "name";
     public static final String KEY_EMAIL = "email";
-    public static final String Refer_Code="code";
 
 }
--- app/src/main/java/com/ujjwalagrawal/spectrum/profile/view/ProfileFragment.java
+++ app/src/main/java/com/ujjwalagrawal/spectrum/profile/view/ProfileFragment.java
@@ -2,30 +2,21 @@ package com.ujjwalagrawal.spectrum.profile.view;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ActionProvider;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.ujjwalagrawal.spectrum.R;
-import com.ujjwalagrawal.spectrum.helper.Keys;
 
 import static com.ujjwalagrawal.spectrum.R.id.tabLayout;
 
@@ -47,44 +38,6 @@ public class ProfileFragment extends Fragment {
 
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-
-        inflater.inflate(R.menu.profile, menu);
-        MenuItem menuItem = menu.findItem(R.id.action_share);
-        super.onCreateOptionsMenu(menu, inflater);
-
-
-    }
-
-    @Override
-    public void onPrepareOptionsMenu(Menu menu) {
-        super.onPrepareOptionsMenu(menu);
-        MenuItem menuShare = menu.findItem(R.id.action_share);
-        ShareActionProvider shareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(menuShare);
-        shareAction.setShareIntent(setShareActionIntent(("Please install the app using my referral code to let me win a " +
-                " special prize in spectrum 2018.Use this referral code "+ Keys.Refer_Code +"after installing the app.")));
-    }
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()){
-//            case R.id.action_share:
-//
-//                return true;
-//            default:
-//                return super.onOptionsItemSelected(item);
-//
-//        }
-
-//    }
-    public Intent setShareActionIntent(String text) {
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT, text);
-        return intent;
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
          View view = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -92,11 +45,9 @@ public class ProfileFragment extends Fragment {
         mpager=(ViewPager)view.findViewById(R.id.viewPager);
         toolbar=(Toolbar)view.findViewById(R.id.toolbar);
 
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
         mpager.setAdapter(madapter);
         tabLayout=(TabLayout)view.findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mpager);
-
 
 
         return view;
