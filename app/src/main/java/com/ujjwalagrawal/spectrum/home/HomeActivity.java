f8fab747a5b56bb1a123e6f49285cc45217cb4dc
Shubham Raj <shubhamismrocker@gmail.com>
2017-12-08 00:42:14 +0530

    resolved conflicts

--- app/src/main/java/com/ujjwalagrawal/spectrum/home/HomeActivity.java
+++ app/src/main/java/com/ujjwalagrawal/spectrum/home/HomeActivity.java
@@ -20,80 +20,17 @@ import com.ujjwalagrawal.spectrum.R;
 import com.ujjwalagrawal.spectrum.events.view.EventTitleListFragment;
 import com.ujjwalagrawal.spectrum.profile.view.ProfileFragment;
 import com.ujjwalagrawal.spectrum.sponsorship.view.SponsorsFragment;
-<<<<<<< HEAD
+
 import com.ujjwalagrawal.spectrum.team.view.TeamFragment;
-=======
+
 import com.ujjwalagrawal.spectrum.teams.TeamsFragment;
->>>>>>> master
 
 import io.fabric.sdk.android.Fabric;
 
+import static com.ujjwalagrawal.spectrum.R.menu.profile;
+
 public class HomeActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link FragmentPagerAdapter} derivative, which will keep every
-	 * loaded fragment in memory. If this becomes too memory intensive, it
-	 * may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
-
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Fabric.with(this, new Crashlytics());
-		setContentView(R.layout.activity_home);
-		BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-		bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-			@Override
-			public void onTabSelected(@IdRes int tabId) {
-				if (tabId == R.id.tab_profile) {
-
-					// The tab with id R.id.tab_favorites was selected,
-					// change your content accordingly.
-					ProfileFragment profile =new ProfileFragment();
-					setFragment(profile);
-				}
-				else if (tabId == R.id.tab_events){
-					EventTitleListFragment eventTitleListFragment = new EventTitleListFragment();
-					setFragment(eventTitleListFragment);
-				}
-				else if (tabId == R.id.tab_home){
-					HomeFragment homeFragment = new HomeFragment();
-					setFragment(homeFragment);
-
-				}
-				else if (tabId == R.id.tab_sponsors){
-					SponsorsFragment sponsorsFragment = new SponsorsFragment();
-					setFragment(sponsorsFragment);
-
-
-				}
-				else if (tabId == R.id.tab_aboutus){
-
-					TeamFragment teamFragment = new TeamFragment();
-					setFragment(teamFragment);
-				}
-
-			}
-		});
-	}
-
-	public void setFragment(Fragment fragment) {
-		if (fragment != null) {
-			FragmentManager fragmentManager = getSupportFragmentManager();
-			FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-			fragmentTransaction.replace(R.id.contentContainer, fragment);
-			fragmentTransaction.commit();
-		}
-	}
-=======
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -107,25 +44,11 @@ public class HomeActivity extends AppCompatActivity {
      * The {@link ViewPager} that will host the section contents.
      */
 
-    private VideoView video1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_home);
-
-        video1 = (VideoView) findViewById(R.id.video);//ADDED AVIDEO BACKGROUND IN THE BASE ACTIVITY
-        Uri uri = Uri.parse("android.resource://" + "com.ujjwalagrawal.spectrum" + "/" + R.raw.test);
-        video1.setVideoURI(uri);
-        video1.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                video1.start();
-                mediaPlayer.setLooping(true);
-            }
-        });
-
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
@@ -134,6 +57,8 @@ public class HomeActivity extends AppCompatActivity {
 
                     // The tab with id R.id.tab_favorites was selected,
                     // change your content accordingly.
+                    ProfileFragment profile = new ProfileFragment();
+                    setFragment(profile);
                 } else if (tabId == R.id.tab_events) {
                     EventTitleListFragment eventTitleListFragment = new EventTitleListFragment();
                     setFragment(eventTitleListFragment);
@@ -147,9 +72,9 @@ public class HomeActivity extends AppCompatActivity {
 
 
                 } else if (tabId == R.id.tab_aboutus) {
-                    TeamsFragment teamsFragment = new TeamsFragment();
-                    setFragment(teamsFragment);
 
+                    TeamFragment teamFragment = new TeamFragment();
+                    setFragment(teamFragment);
                 }
 
             }
@@ -164,5 +89,4 @@ public class HomeActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
     }
->>>>>>> master
 }
