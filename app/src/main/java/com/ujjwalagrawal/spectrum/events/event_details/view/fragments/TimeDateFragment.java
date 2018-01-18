2c2d2bd8b62baea4a38789bab9757f95be243104
ujjwalagrawal17 <ujjwal.iitism@gmail.com>
2018-01-19 02:46:46 +0530

    Event Details Module Added

--- app/src/main/java/com/ujjwalagrawal/spectrum/events/event_details/view/fragments/TimeDateFragment.java
+++ app/src/main/java/com/ujjwalagrawal/spectrum/events/event_details/view/fragments/TimeDateFragment.java
@@ -1,4 +1,4 @@
-package com.ujjwalagrawal.spectrum.events.event_details.view;
+package com.ujjwalagrawal.spectrum.events.event_details.view.fragments;
 
 
 import android.os.Bundle;
@@ -9,67 +9,64 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ujjwalagrawal.spectrum.R;
+import com.ujjwalagrawal.spectrum.events.event_details.data.EventDetails;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-/**
- * A simple {@link Fragment} subclass.
- */
+
 public class TimeDateFragment extends Fragment {
-    
-    
+
+
     @BindView(R.id.time_textview)
     TextView timeTextview;
     @BindView(R.id.date_textview)
     TextView dateTextview;
     @BindView(R.id.location_textview)
     TextView locationTextview;
-    
+
     String date = "";
     String time = "";
     String location = "";
-    
+
     public TimeDateFragment() {
     }
-    
-    public static TimeDateFragment newInstance(String date,String time) {
-        
+
+    public static TimeDateFragment newInstance() {
+
         Bundle args = new Bundle();
-        
+
         TimeDateFragment fragment = new TimeDateFragment();
         fragment.setArguments(args);
-        args.putString("date",date);
-        args.putString("time",time);
         return fragment;
     }
-    
-    
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_time_date, container, false);
         ButterKnife.bind(this, view);
-//        date=;
-//        time=;
+
         timeTextview.setText(time);
         dateTextview.setText(date);
         locationTextview.setText(location);
-        
+
         return view;
     }
-    
-//    public void update(Event event) {
-//        date = event.getDate();
-//        time = event.getTime();
-//        location = event.getLocation();
-//
-//        if(timeTextview == null){
-//            return;
-//        }
-//
-//        timeTextview.setText(time);
-//        dateTextview.setText(date);
-//        locationTextview.setText(location);
-//    }
+
+    public void update(EventDetails eventDetails) {
+        date = eventDetails.getDate();
+        time = eventDetails.getTime();
+        location = eventDetails.getLocation();
+
+        if(timeTextview == null){
+            return;
+        }
+
+        timeTextview.setText(time);
+        dateTextview.setText(date);
+        locationTextview.setText(location);
+    }
+
 }
